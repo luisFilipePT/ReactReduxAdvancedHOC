180a0904025b2eeca61add41c205a8aeae8414ea
Luís <lf@void.pt>
2017-08-18 17:35:07 +0100

    first commit

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return (
+      <div>
+          Super Special Secret Recipe
+          <ul>
+              <li>1 cup of sugar</li>
+              <li>1 cup of salt</li>
+              <li>1 cup of peper</li>
+          </ul>
+      </div>
+    );
+}
